cd84c1460811bfa91d9bd5ac9dfb2f1fc32a80e6
alexvolchkov <volkaa@mail.ru>
2021-12-26 15:56:12 +0300

    5.1.2. Создать итератор четные числа [#150]

---
+++ src/main/java/ru/job4j/it/EvenNumbersIterator.java
@@ -0,0 +1,33 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+    private int[] data;
+    private int index;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (isHasNext() && data[index] % 2 != 0) {
+            index++;
+        }
+        return isHasNext();
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[index++];
+    }
+
+    private boolean isHasNext() {
+        return index < data.length;
+    }
+}
