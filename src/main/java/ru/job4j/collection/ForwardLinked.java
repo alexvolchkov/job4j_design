99297b3f7e022457fb0923321893e0332df74fb9
alexvolchkov <volkaa@mail.ru>
2022-01-04 10:14:01 +0300

    5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 
 public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
+    private int size;
 
     public void add(T value) {
         Node<T> node = new Node<T>(value, null);
@@ -17,6 +18,7 @@ public class ForwardLinked<T> implements Iterable<T> {
             tail = tail.next;
         }
         tail.next = node;
+        size++;
     }
 
     @Override
@@ -48,12 +50,18 @@ public class ForwardLinked<T> implements Iterable<T> {
         Node<T> oldHead = head;
         head = head.next;
         oldHead.next = null;
+        size--;
         return oldHead.value;
     }
 
     public void addFirst(T value) {
         head = new Node<>(value, head);
+        size++;
      }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
     private static class Node<T> {
         T value;
