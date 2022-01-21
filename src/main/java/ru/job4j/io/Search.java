66a616604494a0f67b5bfa997a0de4c11ff9d5ac
alexvolchkov <volkaa@mail.ru>
2022-01-21 22:54:17 +0300

    5. Валидация параметров запуска. [#246865 #253446]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,14 +9,9 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder and filter are null. "
-                    + "Usage java -jar dir.jar ROOT_FOLDER FILTER.");
-        } else if (args.length == 1) {
-            throw new IllegalArgumentException("Filter is null. "
-                    + "Usage java -jar dir.jar ROOT_FOLDER FILTER.");
-        }
+        validationAmountArgument(args);
         Path start = Paths.get(args[0]);
+        validationExistFolder(start);
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
@@ -24,5 +19,23 @@ public class Search {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validationAmountArgument(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder and filter are null. "
+                    + "Usage java -jar dir.jar ROOT_FOLDER FILTER.");
+        } else if (args.length == 1) {
+            throw new IllegalArgumentException("Filter is null. "
+                    + "Usage java -jar dir.jar ROOT_FOLDER FILTER.");
+        }
+    }
+
+    public static void validationExistFolder(Path folder) {
+        if (!Files.exists(folder)) {
+            throw new IllegalArgumentException(String.format("Not exist %s", folder.toAbsolutePath()));
+        } else if (!Files.isDirectory(folder)) {
+            throw new IllegalArgumentException(String.format("Not directory %s", folder.toAbsolutePath()));
+        }
     }
 }
