e7f52e5d4ec7c2a78c616219f7363c69e37dd9e1
alexvolchkov <volkaa@mail.ru>
2022-01-29 16:40:40 +0300

    0. Select [#6861]

---
+++ script/lessonSelect/insert.sql
@@ -0,0 +1,15 @@
+insert into fauna (name, avg_age, discovery_date) values ('fish 1', 3600, '1758-1-1');
+insert into fauna (name, avg_age, discovery_date) values ('fish 2', 10000, '1823-3-1');
+insert into fauna (name, avg_age, discovery_date) values ('fish 3', 34600, '2020-1-1');
+insert into fauna (name, avg_age) values ('fish 4', 5000);
+insert into fauna (name, avg_age, discovery_date) values ('fish 5', 20000, '1958-10-1');
+insert into fauna (name, avg_age, discovery_date) values ('bird 1', 10, '1500-1-1');
+insert into fauna (name, avg_age, discovery_date) values ('bird 2', 5000, '1980-1-1');
+insert into fauna (name, avg_age, discovery_date) values ('bird 3', 33000, '1920-1-1');
+insert into fauna (name, avg_age, discovery_date) values ('bird 4', 100000, '2000-3-4');
+insert into fauna (name, avg_age) values ('bird 5', 3600);
+insert into fauna (name, avg_age, discovery_date) values ('mammal 1', 3600, '1750-1-1');
+insert into fauna (name, avg_age, discovery_date) values ('mammal 2', 10000, '1950-1-1');
+insert into fauna (name, avg_age, discovery_date) values ('mammal 3', 15000, '1980-1-1');
+insert into fauna (name, avg_age, discovery_date) values ('mammal 4', 50000, '1879-1-1');
+insert into fauna (name, avg_age, discovery_date) values ('mammal 5', 100000, '2000-1-1');
