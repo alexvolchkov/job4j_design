903c5b235824484184acde21bf148ba55eab2725
alexvolchkov <volkaa@mail.ru>
2022-02-07 22:14:24 +0300

    0.1. Statement [#379306]

---
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,123 @@
+package ru.job4j.jdbc;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+import java.util.StringJoiner;
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+    private Properties properties;
+
+    public TableEditor(Properties properties) throws ClassNotFoundException, SQLException {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(properties.getProperty("nameClass.idea_db"));
+        connection = DriverManager.getConnection(
+                properties.getProperty("url.idea_db"),
+                properties.getProperty("login.idea_db"),
+                properties.getProperty("password.idea_db")
+        );
+    }
+
+    public void createTable(String tableName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format("create table if not exists %s();", tableName);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dropTable(String tableName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format("drop table if exists %s;", tableName);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addColumn(String tableName, String columnName, String type) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format("alter table if exists %s add column if not exists %s %s;",
+                    tableName, columnName, type);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dropColumn(String tableName, String columnName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format("alter table if exists %s drop column if exists %s;",
+                    tableName, columnName);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format("alter table if exists %s rename column %s to %s;",
+                    tableName, columnName, newColumnName);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "select * from %s limit 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+        try (FileReader fr = new FileReader("app.properties")) {
+            properties.load(fr);
+        } catch (FileNotFoundException fne) {
+            fne.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        TableEditor tableEditor = new TableEditor(properties);
+        tableEditor.createTable("demo_table");
+        System.out.println(TableEditor.getTableScheme(tableEditor.connection, "demo_table"));
+        tableEditor.addColumn("demo_table", "test_column", "varchar(20)");
+        System.out.println(TableEditor.getTableScheme(tableEditor.connection, "demo_table"));
+        tableEditor.renameColumn("demo_table", "test_column", "new_test_column");
+        System.out.println(TableEditor.getTableScheme(tableEditor.connection, "demo_table"));
+        tableEditor.dropColumn("demo_table", "new_test_column");
+        System.out.println(TableEditor.getTableScheme(tableEditor.connection, "demo_table"));
+        tableEditor.dropTable("demo_table");
+    }
+}
