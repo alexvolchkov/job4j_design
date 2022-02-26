fd3578898a6369eddbc722f93df37cd018d7c164
alexvolchkov <volkaa@mail.ru>
2022-02-26 19:02:58 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class GeneratorTest {
+
+    Generator generator = new SimpleGenerator();
+
+    @Test
+    public void whenProduce() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Alex");
+        args.put("subject", "you");
+        String expect = "I am a Alex, Who are you?";
+        assertThat(generator.produce(template, args), is(expect));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenEnoughArguments() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Alex");
+        String result = generator.produce(template, args);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenUnnecessaryArguments() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Alex");
+        args.put("subject", "you");
+        args.put("surname", "Ivanov");
+        String result = generator.produce(template, args);
+    }
+}
