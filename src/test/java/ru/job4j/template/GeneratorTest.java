6bd2563eb6e32f424e77db701cea0f918f64d5b5
alexvolchkov <volkaa@mail.ru>
2022-02-27 15:59:44 +0300

    3. Шаблонизатор. [#855 #269773]

--- src/test/java/ru/job4j/template/GeneratorTest.java
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 
 public class GeneratorTest {
 
-    Generator generator = new SimpleGenerator();
+   private Generator generator = new SimpleGenerator();
 
     @Ignore
     @Test
