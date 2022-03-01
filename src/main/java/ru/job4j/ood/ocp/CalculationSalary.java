23f03a9d9e9d7a6d902c24be96bb14273af42140
alexvolchkov <volkaa@mail.ru>
2022-03-01 18:09:08 +0300

    0. Принцип открытости закрытости [#4914]

---
+++ src/main/java/ru/job4j/ood/ocp/CalculationSalary.java
@@ -0,0 +1,11 @@
+package ru.job4j.ood.ocp;
+
+/*Если нужно будет рассчитывать зарплату по другому графику, то придется переписывать метод.  */
+
+public class CalculationSalary {
+
+    public double calculate() {
+        /*Расчет зарплаты по пятидневке */
+        return 0;
+    }
+}
