b9caec6d4e1461e18dd1be8a925b3cea02674538
alexvolchkov <volkaa@mail.ru>
2022-03-13 17:22:07 +0300

    2. Парковка машин [#853 #273453]

--- src/main/java/ru/job4j/ood/lsp/parking/CarTruckParking.java
+++ src/main/java/ru/job4j/ood/lsp/parking/CarTruckParking.java
@@ -8,7 +8,6 @@ public class CarTruckParking implements Parkingable {
     private int truckSpace;
     private int freeCarSpace;
     private int freeTruckSpace;
-    private static final int CAR_SIZE = 1;
     private Map<Vehicle, Integer> parking = new HashMap<>();
 
     public CarTruckParking(int carSpace, int truckSpace) {
@@ -35,14 +34,14 @@ public class CarTruckParking implements Parkingable {
     }
 
     public Map<Vehicle, Integer> getParking() {
-        return parking;
+        return new HashMap<>(parking);
     }
 
     @Override
     public boolean park(Vehicle vehicle) {
         boolean rsl = false;
-        if (vehicle.getSize() > CAR_SIZE && freeTruckSpace > 0) {
-            parking.put(vehicle, 1);
+        if (vehicle.getSize() > Car.CAR_SIZE && freeTruckSpace > 0) {
+            parking.put(vehicle, vehicle.getSize());
             freeTruckSpace--;
             rsl = true;
         } else if (freeCarSpace >= vehicle.getSize()) {
