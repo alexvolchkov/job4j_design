eab1e807f62c687957482323f2ea54350bd32ec7
alexvolchkov <94671261+alexvolchkov@users.noreply.github.com>
2023-10-19 23:00:18 +0300

    7. Балансир [#505083]

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,25 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Aleksandr Volchkov
+ */
+public class Balancer {
+
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        if (nodes == null || nodes.isEmpty()) {
+            throw new IllegalArgumentException("Nodes %s should not be empty".formatted(nodes));
+        }
+        for (int i = 0; i < nodes.size();) {
+            if (source.hasNext()) {
+                nodes.get(i).add(source.next());
+                i = i == nodes.size() - 1 ? 0 : i + 1;
+            } else {
+                break;
+            }
+        }
+    }
+}
